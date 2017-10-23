4368fe551ad8f42b292c9826a1d95e4fa88d4793
Vipin Vijayan <vvjnvt@gmail.com>
2017-10-22 23:25:43 -0400

    Added snapshots methods

--- src/NetalignUtils.jl
+++ src/NetalignUtils.jl
@@ -28,10 +28,10 @@ end
 Convert dynamic network to static network
 where each dynamic edge corresponds to a static edge with weight 1
 """
-flatten(nt::Network) = nt
 flatten(G::SparseMatrixCSC{<:Number,Int}) = G
 flatten(G::SparseMatrixCSC{Events,Int}) =
     SparseMatrixCSC(G.m,G.n,G.colptr,G.rowval,ones(Int,length(G.nzval)))
+flatten(nt::Network) = nt
 flatten(dy::DynamicNetwork) = Network(flatten(dy.G), dy.nodes)
 
 function events2dynet(I, J, V, n, nodes; makesymmetric=true)
@@ -41,6 +41,43 @@ function events2dynet(I, J, V, n, nodes; makesymmetric=true)
         G = sparse(I,J, V, n, n, fixevents)
     end
     DynamicNetwork(G,nodes)
+end
+
+"""
+Convert snapshots into dynamic network with event duration 1
+and time starting at 0
+"""
+function snapshots2dynet(snaps::Vector{Network};
+                                 makesymmetric=false,sortby=nothing)
+    nodes = Set{String}()
+    events = Vector{Tuple{String,String,Float64,Float64}}()
+    for t = 1:length(snaps)
+        Gc = snaps[t].G
+        nodesc = snaps[t].nodes
+        Ic,Jc,Vc = findnz(triu(Gc,1))
+        append!(events, collect(zip(nodesc[Ic],nodesc[Jc],
+                                    fill(Float64(t-1),length(Vc)),
+                                    fill(Float64(t),length(Vc)))))
+        union!(nodes,nodesc)
+    end
+    nodes = collect(nodes)
+    if sortby!=nothing
+        nodes = sort(nodes,by=sortby)
+    end
+    nodesnum = Dict(node => i for (i,node) = enumerate(nodes))
+    edges = Matrix{Int}(length(events),2)
+    timestamps = Vector{Vector{Tuple{Float64,Float64}}}(length(events))
+    for i = 1:length(events)
+        vals = events[i]
+        n1 = nodesnum[vals[1]]
+        n2 = nodesnum[vals[2]]
+        edges[i,1] = min(n1,n2)
+        edges[i,2] = max(n1,n2)
+        timestamps[i] = [(vals[3],vals[4])]
+    end
+    events2dynet(edges[:,1], edges[:,2], timestamps,
+                 length(nodes), nodes,
+                 makesymmetric=makesymmetric)
 end
 
 include("ionet.jl")
